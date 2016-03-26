93f777c7b80aa5f2aa2f04b373b43a11dd0ef3bd
ok2ju <soulSpringg@gmail.com>
2016-03-26 19:51:47 +0300

    some upload features

--- client/app/components/segments/segments.controller.js
+++ client/app/components/segments/segments.controller.js
@@ -1,45 +1,28 @@
 function SegmentsController(Upload, $timeout, $scope) {
   const vm = this;
 
-  $scope.$watch('files', function () {
-        vm.upload(vm.files);
-    });
-    $scope.$watch('file', function () {
-        if (vm.file != null) {
-            vm.files = [vm.file]; 
-        }
-    });
-    vm.log = '';
+  vm.upload = function (files) {
+    vm.files = files;
 
-    vm.upload = function (files) {
-        if (files && files.length) {
-            for (var i = 0; i < files.length; i++) {
-              var file = files[i];
-              if (!file.$error) {
-                Upload.upload({
-                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                    data: {
-                      username: vm.username,
-                      file: file  
-                    }
-                }).then(function (resp) {
-                    $timeout(function() {
-                        vm.log = 'file: ' +
-                        resp.config.data.file.name +
-                        ', Response: ' + JSON.stringify(resp.data) +
-                        '\n' + $scope.log;
-                    });
-                }, null, function (evt) {
-                    var progressPercentage = parseInt(100.0 *
-                        evt.loaded / evt.total);
-                    vm.log = 'progress: ' + progressPercentage + 
-                      '% ' + evt.config.data.file.name + '\n' + 
-                      vm.log;
-                });
-              }
-            }
-        }
-    };
+    angular.forEach(files, (file) => {
+      file.upload = Upload.upload({
+        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+        data: { file: file }
+      });
+
+      file.upload.then((response) => {
+        $timeout(function() {
+            file.result = response.data;
+        });
+      }, function(response) {
+        if (response.status > 0) {
+          $scope.errorMsg = response.status + ': ' + response.data;
+        }    
+      }, function(evt) {
+        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      });
+    });
+  };
 }
 
 SegmentsController.$inject = ['Upload', '$timeout', '$scope'];

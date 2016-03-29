d868bdca65d7611e66b47a49b4f33bedd536c888
ok2ju <soulSpringg@gmail.com>
2016-03-29 09:48:58 +0300

    fix segments dropping

--- client/app/components/podcast/podcast.controller.js
+++ client/app/components/podcast/podcast.controller.js
@@ -5,6 +5,30 @@ function PodcastController(segments) {
 
   vm.segments = segments;
   vm.tracks = [];
+  vm.dropCallback = dropCallback;
+
+
+  function dropCallback(event, index, item, external, type, allowedType) {
+      console.log('event:');
+      console.log(event);
+      console.log('index:');
+      console.log(index);
+      console.log('external:');
+      console.log(external);
+      console.log('type:');
+      console.log(type);
+      console.log('item');
+      console.log(item);
+      var index = _.findIndex(vm.segments, (o) => {
+        return o.id === item.id;
+      });
+
+      if(index >= 0) {
+        vm.segments.splice(index, 1);
+      } 
+      
+      return item;
+  };
 }
 
 export default PodcastController;
